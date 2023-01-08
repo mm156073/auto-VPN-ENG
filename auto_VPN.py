ec0cf75bb7591b51526c1e3c6fc6a6e30c2084ce
mm156073 <156073asdzxc@gmail.com>
2023-01-09 00:13:04 +0900

    パラメータをyamlで管理

--- auto_VPN.py
+++ auto_VPN.py
@@ -6,12 +6,11 @@ import subprocess
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-from params import *
+import yaml
 
 URL = "https://tauth-plgw1.t.u-tokyo.ac.jp/ui/"
 
-def get_onetime_pass() -> List:
+def get_onetime_pass(params) -> List:
     pattern_list = []
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
@@ -21,7 +20,7 @@ def get_onetime_pass() -> List:
     browser.get(URL)
 
     user_id = browser.find_element(By.ID, "uid")
-    user_id.send_keys(ID)
+    user_id.send_keys(params["ID"])
     submit = browser.find_element(By.CLASS_NAME, "passlogic_css_submit")
     submit.click()
 
@@ -33,18 +32,20 @@ def get_onetime_pass() -> List:
         for box in boxes:
             num = box.find_element(By.TAG_NAME, "p")
             pattern_list.append(num.text)
-    password_list = [pattern_list[idx] for idx in PATTERN]
+    password_list = [pattern_list[idx] for idx in params["PATTERN"]]
     password = ""
     for string in password_list:
         password += string
     return password
 
-def main():
-    password = "vpn.secrets.password:" + get_onetime_pass() + SECRET 
-    with open(FILE_NAME, "w") as f:
+def main(params):
+    password = "vpn.secrets.password:" + get_onetime_pass(params) + params["SECRET"]
+    with open(params["FILE_NAME"], "w") as f:
         f.write(password)
-    res = subprocess.run(["nmcli", "con", "up", "id", VPN, "passwd-file", FILE_NAME])
-    os.remove(FILE_NAME)
+    res = subprocess.run(["nmcli", "con", "up", "id", params["VPN"], "passwd-file", params["FILE_NAME"]])
+    os.remove(params["FILE_NAME"])
 
 if __name__ == "__main__":
-    main()
+    with open('params.yaml') as file:
+        params = yaml.safe_load(file)
+    main(params)
